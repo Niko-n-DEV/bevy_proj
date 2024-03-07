4e74f725aa98c1d610301023756df199981560e2
Niko-n-DEV <nikoalr6@gmail.com>
2024-03-07 11:36:17 +0300

    CARGO FMT

--- src/systems/world/TileMap.rs
+++ src/systems/world/TileMap.rs
@@ -2,18 +2,10 @@
 use bevy::prelude::*;
 use bevy_entitiles::*;
 
-
 pub struct TileMapPlugin;
 
 impl Plugin for TileMapPlugin {
-    fn build(&self, app: &mut App) {
-        
-    }
+    fn build(&self, app: &mut App) {}
 }
 
-fn setup(
-    mut commands: Commands, 
-    asset_server: Res<AssetServer>
-) {
-
-}
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {}
